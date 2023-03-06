e038f8fc9ea7f2d33a65d03f1ba96643c93c9dc3
SpunkyWonkle <118027686+SpunkyWonkle@users.noreply.github.com>
2023-03-06 17:40:26 -0500

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCaCCxlc_TuzIAFlCwpFyM4bJkoOyaIC2w",
+    authDomain: "sussierthanmost.firebaseapp.com",
+    databaseURL: "https://sussierthanmost-default-rtdb.firebaseio.com",
+    projectId: "sussierthanmost",
+    storageBucket: "sussierthanmost.appspot.com",
+    messagingSenderId: "972241832131",
+    appId: "1:972241832131:web:5df45222dfa1f104ea7a76"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
